72c43ce200de1ff9467ec3050e8b80cdbad5fe15
Arturstriker3 <artur.danielgama@gmail.com>
2024-07-19 10:27:48 -0300

    feat: home, login and register views

---
+++ src/core/pages/home/home.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+const msg = 'Home Page';
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen w-screen" >
+    <h1 class="bg-gray-400 p-2 border rounded-2xl" >
+      {{ msg }}
+    </h1>
+  </div>
+</template>
--- src/core/router/pages.ts
+++ src/core/router/pages.ts
@@ -1,22 +1,27 @@
-import pageNotFound from "@/core/pages/errors/404.vue"
 import { userAuthStore } from '../stores/auth';
 import { interfacePages } from '../interface/interface';
+
+import pageNotFound from "@/core/pages/errors/404.vue"
+import Login from "@/core/pages/login/login.vue";
+import Register from "@/core/pages/register/register.vue"
 
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: "/login", component: Login },
+        { path: "/register", component: Register },
         { path: "/:pathMatch(.*)", component: pageNotFound },
     ]
 });
 
-router.beforeEach((to, _, next) => {
+router.beforeEach((to: any, _: any, next: any) => {
     const authStore = userAuthStore();
     const targetPage = interfacePages.find(page => page.path === to.path);
 
     if (targetPage && targetPage.showWhenAuth && !authStore.GetIsAuth) {
-        next('#####');
+        next('/');
     } else {
         next();
     }

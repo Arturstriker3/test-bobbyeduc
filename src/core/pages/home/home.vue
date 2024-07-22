bede677dc45473980194b20bfc65124903164b57
Arturstriker3 <artur.danielgama@gmail.com>
2024-07-22 10:56:41 -0300

    feat: pizza chart for name distribution

--- src/core/pages/home/home.vue
+++ src/core/pages/home/home.vue
@@ -6,16 +6,24 @@ import { createToaster } from "@meforma/vue-toaster";
 const isLoading = ref(false);
 const toaster = createToaster();
 
-const totalUsers = ref(0);
+const userData = ref<any[]>([]);
+const firstNames = ref<Record<string, number>>({});
+const lastNames = ref<Record<string, number>>({});
 
 const getData = () => {
   isLoading.value = true;
   usersCrud.getUsers({ page: 1, per_page: 9999999 })
     .then((response) => {
       const users = response.data.data;
-      totalUsers.value = users.length;
+      userData.value = users;
 
-      chartOptions.value.series[0].data = [totalUsers.value];
+      updateFirstNames(users);
+      updateLastNames(users);
+      updateBarChart(users);
+      updatePieChartByFirstName();
+      updatePieChartByLastName();
+
+      console.log(firstNames);
     })
     .catch(() => {
       toaster.error('Falha ao carregar os usuários.');
@@ -23,7 +31,63 @@ const getData = () => {
     .finally(() => { isLoading.value = false; });
 }
 
-const chartOptions = ref({
+const updateFirstNames = (users: any[]) => {
+  firstNames.value = users.reduce((acc, user) => {
+    acc[user.first_name] = (acc[user.first_name] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+};
+
+const updateLastNames = (users: any[]) => {
+  lastNames.value = users.reduce((acc, user) => {
+    acc[user.last_name] = (acc[user.last_name] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+};
+
+const updateBarChart = (users: any[]) => {
+  chartOptions1.value.series[0].data = [users.length];
+}
+
+const updatePieChartByFirstName = () => {
+  chartOptions2.value = {
+    chart: {
+      type: 'pie'
+    },
+    series: Object.values(firstNames.value),
+    labels: Object.keys(firstNames.value),
+    title: {
+      text: 'Distribuição de Nomes',
+      align: 'center',
+      style: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+        color: '#333'
+      }
+    }
+  };
+};
+
+const updatePieChartByLastName = () => {
+  chartOptions3.value = {
+    chart: {
+      type: 'pie'
+    },
+    series: Object.values(lastNames.value),
+    labels: Object.keys(lastNames.value),
+    title: {
+      text: 'Distribuição de Sobrenomes',
+      align: 'center',
+      style: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+        color: '#333'
+      }
+    }
+  };
+};
+
+const chartOptions1 = ref({
   chart: {
     type: 'bar'
   },
@@ -33,21 +97,116 @@ const chartOptions = ref({
   }],
   xaxis: {
     categories: ['Total de Usuários']
+  },
+  title: {
+    text: 'Total de Usuários',
+    align: 'center',
+    style: {
+      fontSize: '16px',
+      fontWeight: 'bold',
+      color: '#333'
+    }
+  }
+});
+
+const chartOptions2 = ref<{
+  chart: {
+    type: 'pie';
+  };
+  series: number[];
+  labels: string[];
+  title: {
+    text: string;
+    align: string;
+    style: {
+      fontSize: string;
+      fontWeight: string;
+      color: string;
+    }
+  };
+}>({
+  chart: {
+    type: 'pie'
+  },
+  series: [],
+  labels: [],
+  title: {
+    text: 'Distribuição de Nomes',
+    align: 'center',
+    style: {
+      fontSize: '16px',
+      fontWeight: 'bold',
+      color: '#333'
+    }
+  }
+});
+
+const chartOptions3 = ref<{
+  chart: {
+    type: 'pie';
+  };
+  series: number[];
+  labels: string[];
+  title: {
+    text: string;
+    align: string;
+    style: {
+      fontSize: string;
+      fontWeight: string;
+      color: string;
+    }
+  };
+}>({
+  chart: {
+    type: 'pie'
+  },
+  series: [],
+  labels: [],
+  title: {
+    text: 'Distribuição de Sobrenomes',
+    align: 'center',
+    style: {
+      fontSize: '16px',
+      fontWeight: 'bold',
+      color: '#333'
+    }
   }
 });
 
 onMounted(() => {
   getData();
 });
+
 </script>
 
+
 <template>
-  <div class="flex justify-center items-center h-screen w-screen">
-    <apexchart
-      width="500"
-      type="bar"
-      :options="chartOptions"
-      :series="chartOptions.series"
-    ></apexchart>
+  <div class="">
+    <div class="w-full max-w-md">
+      <apexchart
+        width="500"
+        type="bar"
+        :options="chartOptions1"
+        :series="chartOptions1.series"
+      ></apexchart>
+    </div>
+    <div class="w-full max-w-md">
+      <apexchart
+        width="500"
+        type="pie"
+        :options="chartOptions2"
+        :series="chartOptions2.series"
+        :labels="chartOptions2.labels"
+      ></apexchart>
+    </div>
+    <div class="w-full max-w-md">
+      <apexchart
+        width="500"
+        type="pie"
+        :options="chartOptions3"
+        :series="chartOptions3.series"
+        :labels="chartOptions3.labels"
+      ></apexchart>
+    </div>
   </div>
 </template>
